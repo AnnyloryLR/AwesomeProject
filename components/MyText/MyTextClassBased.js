0ac6608b13be695d37112e8d21dc70687caab6c1
Annylory LR <annyloryrosa@gmail.com>
2026-05-21 11:48:34 -0300

    lyfe cycle class based component

--- components/MyText/MyTextClassBased.js
+++ components/MyText/MyTextClassBased.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
-import styles from './style';
+import {Text, View} from 'react-native';
 
 class MyTextClassBased extends Component {
   
   constructor(props){
     super(props);
     this.state = {
-
+        fullName:props.name + " Rosa"
     }
+  }
+  componentDidMount(){
+    console.log("Our component has been mounted");
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    console.log("Our component has been updated",
+      prevProps,
+      this.state);
+  }
+
+  componentWillUnmount(){
+    console.log("our component will be unmounted or destroyed");
   }
 
   render() {
-    return(
-        <Text style={{color:'#86d79b', textAlign:'center', fontSize:20}}>
-            Hello, {this.props.name}
-        </Text>
+    return (
+        <View style={{marginTop:100}}>
+            <Text style={{color:'#86d79b', textAlign:'center', fontSize:20}}
+            onPress={() => {
+                this.setState({...this.state, ...{fullName:'Annylory Lima Rosa'}} );
+            }}>
+                Hello, {this.props.name}! I know your full name is {this.state.fullName}.
+            </Text>
+        </View>
+        
     )
   }
 }
